1c67d6c1b013a80056fed05b160825198527ad61
yujian <azoux0311@gamil.com>
2025-12-31 17:35:43 +0800

    Update package.json and vite.config.ts for Nitro integration
    - Changed the start script in package.json to use the Nitro server for improved performance.
    - Added Nitro plugin to vite.config.ts to enhance build capabilities and server functionality.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -15,6 +16,7 @@ export default defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro(), 
     viteReact({
       babel: {
         plugins: [
